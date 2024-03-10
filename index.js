b8c85097eeb1173fed5cb0313c0212ce306e68c0
mk47q <mtynkarjw@gmail.com>
2024-03-10 18:08:05 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let search = document.querySelector(".chos");
+let searchBox = document.querySelector(".wrapperH__search__input");
+search.addEventListener("click" , function(){
+    // searchBox.style.width = "30rem"  
+    // searchBox.style.height = "3rem"  
+    // searchBox.style.transition = "all 1s"
+    searchBox.classList.toggle("gg")
+})
